a10de032cdcdcf7e8a6571ca0bb912c194ad3324
adjeteGuru <justinadjev@hotmail.com>
2020-11-22 23:16:19 +0000

    feat: data tranfer objects created

--- FastFoodWebApi/DTOs/OrderUpdateDto.cs
+++ FastFoodWebApi/DTOs/OrderUpdateDto.cs
@@ -1,6 +1,7 @@
 ﻿using FastFoodWebApi.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,24 +13,17 @@ namespace FastFoodWebApi.DTOs
     //}
     public class OrderUpdateDto
     {
-        //public DateTime? CreateAt { get; set; }
-        //private DateTime? createAt = null;
-        //public DateTime CreateAt
-        //{
-        //    get
-        //    {
-        //        return this.createAt.HasValue
-        //           ? this.createAt.Value
-        //           : DateTime.Now;
-        //    }
-
-        //    set { this.createAt = value; }
-        //}
 
         public int Quantity { get; set; }
-        public DateTime DeliveryDate { get; set; }
+
         //public Customer Customer { get; set; }
         public int CustomerId { get; set; }
+
+        public float DeliveryCharge { get; set; }
+        public TimeSpan EstimateTime { get; set; }
+
+        [MaxLength(50)]
+        public string Alergy { get; set; }
         public Status Status { get; set; }
     }
 }
---
+++ FastFoodWebApi/Migrations/20201122172043_PropUpdated.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FastFoodWebApi.Migrations
+{
+    public partial class PropUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
