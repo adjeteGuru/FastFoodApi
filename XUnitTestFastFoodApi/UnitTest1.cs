560ed1844e7faa3e18ca86165120a22070c5e8da
adjeteGuru <justinadjev@hotmail.com>
2021-02-16 01:24:35 +0000

    feat: WebApiTest removed

--- XUnitTestFastFoodApi/UnitTest1.cs
+++ XUnitTestFastFoodApi/UnitTest1.cs
@@ -6,18 +6,39 @@ namespace XUnitTestFastFoodApi
 {
     public class UnitTest1
     {
-        //CategoryController category = new CategoryController();
-        //[Fact]
-        //public void GetReturnsCategory()
+        //private readonly ICategoryRepository _categoryRepository;
+        //private readonly IMapper _mapper;
+        //CategoryController _categoryController;
+        //ICategoryRepository _categoryRepository;
+        //IMapper _mapper;
+
+        //public CatControllerTest(/*ICategoryRepository categoryRepository, IMapper mapper*/)
         //{
-        //    var returnCat = category.GetCategory(2);
-        //    Assert.Equal("Drink", returnCat.Value);
-        //}
+        //_categoryRepository = new CategoryRepository();
+        //_mapper = mapper;
+        // _categoryController = new CategoryController(_categoryRepository, _mapper);
+        // }
+
+
+        // [Fact]
+        // public void Get_WhenCalled_ReturnsOkResult()
+        // {
+        // Act
+        //  var okResult = _categoryController.GetType();
+
+        // Assert
+        //  Assert.IsType<OkObjectResult>(okResult.Result);
+        // }
 
         //[Fact]
-        //public void Test1()
+        //public void Get_WhenCalled_ReturnsAllItems()
         //{
-
+        //    // Act
+        //    var okResult = _categoryController.GetType().Result as OkObjectResult;
+        //    // Assert
+        //    //var items = Assert.IsType<List<ShoppingItem>>(okResult.Value);
+        //    var categories = Assert.IsAssignableFrom<IEnumerable<Category>>(okResult.Value);
+        //    Assert.Equal(pizza, categories.ToList());
         //}
     }
 }
