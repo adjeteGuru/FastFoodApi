caadb676e3f1c97d7f8136a191ae22e2234c04a9
adjeteGuru <justinadjev@hotmail.com>
2021-04-15 17:36:46 +0100

    feat: unit testing

--- XUnitTestFastFoodApi/UnitTests/CategoryControllerTest.cs
+++ XUnitTestFastFoodApi/UnitTests/CategoryControllerTest.cs
@@ -15,47 +15,28 @@ namespace XUnitTestFastFoodApi.UnitTests
 {
     public class CategoryControllerTest
     {
+        private readonly CategoryController _controller;
+        private readonly Mock<ICategoryRepository> _mockRepo;
+        private readonly Mock<IMapper> _mockMapper;  
+        
+        public CategoryControllerTest()
+        {
+            _mockRepo = new Mock<ICategoryRepository>();
+            _mockMapper = new Mock<IMapper>();
+            _controller = new CategoryController(_mockRepo.Object, _mockMapper.Object);
+        }
+
         [Fact]
         public void GetCategories_ShouldReturnAllCategoriesList()
-        {           
-
-            //Arrange
-
-            //TO DO:
-            //Here I need the mocking library to inject a mock object through DI
-            //Them I need to new up the controller to be tested
-
+        {         
            
-            var mockRepo = new Mock<ICategoryRepository>();
-            mockRepo.Setup(c => c.GetCategories())             
-                .Returns((IEnumerable<Category>)new CategoryReadDto[]
-                {
-                    new CategoryReadDto {Id= 1, Name= "Drink"},
-                    new CategoryReadDto {Id= 2, Name ="Kebab"}
-                }.AsEnumerable());
+            var result = _controller.GetAllCategories();
 
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(m => m.Map<IEnumerable<Category>>(mockMapper))            
-                 .Returns(new Category[]
-                 {
-                     new Category {Id = 1, Name = "Drink"},
-                     new Category {Id = 2, Name = "Kebab"}
-                 }.AsEnumerable());              
+            //Assert            
+            var expected = Assert.IsAssignableFrom<ActionResult<IEnumerable<CategoryReadDto>>>(result);
 
+            Assert.Equal(expected.Value, result.Value);            
 
-            var controller = new CategoryController(mockRepo.Object, mockMapper.Object);
-
-            //Act
-            var actual = controller.GetAllCategories();
-            
-
-            //Assert
-            var expected = Assert.IsAssignableFrom<IEnumerable<CategoryReadDto>>(actual);            
-
-
-             Assert.Equal(expected, actual.Value);
-            
-               
         }
     }
 }
